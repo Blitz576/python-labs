f9233586017c765e016b7aa654ea88770e9cafd4
Blitz576 <anagy8050@gmail.com>
2024-03-29 17:04:04 +0200

    First Lab

---
+++ lab1/day1_test.py
@@ -0,0 +1,7 @@
+message = "Hello World This message here is from iti and we all support ghaza!"
+vowels=['a','e','i','o','u']
+cnt=0
+for vowel in vowels:
+    cnt+=(message.count(vowel))
+
+print(cnt);
